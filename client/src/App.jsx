81f646758d004ea4696dd806dce0692edcc077d8
keshav62 <keshavsing1230@gmail.com>
2026-03-26 00:49:25 +0530

    add text prompt and loading page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
 import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
+import Loading from './pages/Loading'
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false); 
+  const {pathname} = useLocation(); 
+
+  if(pathname == '/loading') return <Loading/>
+
+
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert ' onClick={()=> setIsMenuOpen(true)}></img>}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -10,7 +10,7 @@ export const AppContextProvider = ({children}) => {
   const [user, setUser] = useState(null); 
   const [chats, setChats] = useState([]); 
   const [selectedChat, setSelectedChat ] = useState(null); 
-  const [theme, setTheme] = useState(localStorage.getItem ('theme') || ('light')); 
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || ('light')); 
 
   const fetchUser = async()=> { 
     setUser(dummyUserData)
